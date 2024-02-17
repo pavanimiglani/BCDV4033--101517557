176f8c74d69691fb0d7cef8f43cf126758e268f0
Pavani Miglani <pavanimiglani575@gmail.com>
2024-02-16 22:04:19 -0500

    Initial commit

---
+++ tests/sample.test.js
@@ -0,0 +1,11 @@
+// sample.test.js
+
+// A simple test
+test('adds 1 + 2 to equal 3', () => {
+    // Test inputs
+    const sum = 1 + 2;
+  
+    // Expected result
+    expect(sum).toBe(3);
+  });
+  
